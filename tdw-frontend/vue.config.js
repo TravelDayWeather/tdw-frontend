8b54eeb06feff3990cdfee80b11ac13af25378d2
jangseonho <sunho3456@naver.com>
2024-11-04 18:59:48 +0900

    vue.config.js update

--- tdw-frontend/vue.config.js
+++ tdw-frontend/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  build: {
+    outDir: "../tdw-backend/src/main/resources/static",
+  }, // 빌드 결과물이 생성되는 경로
+  server: {
+    proxy: {
+      "/api": "http://localhost:8080",
+    }, // proxy 설정
+  },
 })
