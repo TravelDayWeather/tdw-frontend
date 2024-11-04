300a651365a44e38dc234cf44c352f2b31beefff
jangseonho <sunho3456@naver.com>
2024-11-04 18:30:46 +0900

    test2

--- tdw-fronend/vue.config.js
+++ tdw-fronend/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // npm run build 타켓 디렉토리 (벡엔드쪽)
+  outputDir: "../tdw-backend/src/main/static",
+
+  devServer: {
+    proxy: {
+      '/': {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      }
+    }
+  }
 })
