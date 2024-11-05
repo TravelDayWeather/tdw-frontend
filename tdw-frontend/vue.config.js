1254401a79b03f07edc84ae80221d1920cb3c085
jangseonho <sunho3456@naver.com>
2024-11-05 20:11:52 +0900

    🔗 front-back 연동

--- tdw-frontend/vue.config.js
+++ tdw-frontend/vue.config.js
@@ -1,12 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  build: {
-    outDir: "../tdw-backend/src/main/resources/static",
-  }, // 빌드 결과물이 생성되는 경로
-  server: {
+
+  outputDir: "/Users/jangseonho/tdw-backend/src/main/resources/static",
+
+  devServer: {
     proxy: {
-      "/api": "http://localhost:8080",
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      }
     }, // proxy 설정
   },
 })
