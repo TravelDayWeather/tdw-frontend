ca9fc52071049ee4b9c4e94a7b9c675a0141a5ba
jangseonho <sunho3456@naver.com>
2024-11-06 19:06:29 +0900

    👩‍💻 로그인 (1)

---
+++ tdw-frontend/src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h3 class="login-email-input login-input">email</h3>
+      <input type="text" placeholder="Enter Email" class="login-input-field" required>
+      <h3 class="login-pw-input login-input">password</h3>
+      <input type="text" placeholder="Enter Password" class="login-input-field" required>
+    </div>
+    <div>
+      <button class="login-button" @click="handleLogin">로그인</button>
+    </div>
+    <div>
+      <button class="signup-button" @click="handleLogin">회원가입</button>
+    </div>
+    
+  </template>
+  
+  <script>
+  import '@/assets/css/Login.css';
+
+  export default {
+    name: 'LoginComponent',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
--- tdw-frontend/vue.config.js
+++ tdw-frontend/vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
 
   devServer: {
     proxy: {
-      "/api": {
+      "api/": {
         target: "http://localhost:8080",
         changeOrigin: true,
       }
