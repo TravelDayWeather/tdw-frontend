928ce5c0c9fb24dae7875f3589a723903f37f05e
jangseonho <sunho3456@naver.com>
2024-11-12 19:24:13 +0900

    👩‍💻 로그인 (2)

--- tdw-frontend/src/components/LoginPage.vue
+++ tdw-frontend/src/components/LoginPage.vue
@@ -2,9 +2,9 @@
   <img alt="Vue logo" src="../assets/img/tdw logo.png">
     <div>
       <h3 class="login-email-input login-input">email</h3>
-      <input type="text" placeholder="Enter Email" class="login-input-field" required>
+      <input type="text" v-model="loginRequest.email" placeholder="Enter Email" class="login-input-field" required>
       <h3 class="login-pw-input login-input">password</h3>
-      <input type="text" placeholder="Enter Password" class="login-input-field" required>
+      <input type="password" v-model="loginRequest.pw" placeholder="Enter Password" class="login-input-field" required>
     </div>
     <div>
       <button class="login-button" @click="handleLogin">로그인</button>
@@ -17,12 +17,34 @@
   
 <script>
 import '@/assets/css/Login.css';
+import axios from 'axios';
 
 export default {
-  name: 'LoginPage',
+  data() {
+      return {
+        loginRequest: {
+          email: "",
+          pw: "",
+        },
+      }
+    },
   methods: {
     goToSignUp() {
       this.$router.push('/signup'); // 회원가입 페이지로 이동
+    },
+    async handleLogin() {
+      await axios.post(`api/login`, this.loginRequest, {
+        header: {
+          "Context-Type": "multipart/form-data"
+        }
+      }).then((res) => {
+        console.log(res.data);
+        alert('로그인에 성공했습니다.');
+      }).catch((error) => {
+        console.log(this.loginRequest);
+        console.log(error);
+        alert('로그인에 실패했습니다. 다시 시도해주세요.');
+      })
     }
   }
 }
