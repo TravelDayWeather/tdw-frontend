5c65e07db9797b2e265b1cc9b9b583123e91853f
jangseonho <sunho3456@naver.com>
2024-11-19 00:00:12 +0900

    👩‍💻 마이페이지 (2)

--- tdw-frontend/src/components/Header/HomeViewHeader.vue
+++ tdw-frontend/src/components/Header/HomeViewHeader.vue
@@ -31,7 +31,20 @@ export default{
                 }
             }).then((res) => {
                 console.log(res.data);
-                this.$router.push({name: 'MyPage', params: {userId: res.data.userId} });
+                this.$router.push({name: 'MyPage', 
+                params: {
+                    userId: res.data.userId,            
+                },
+                query: {
+                    userData: JSON.stringify({
+                        name: res.data.name,
+                        email: res.data.email,
+                        nickname: res.data.nickname, // 'nikcname' -> 'nickname' 수정
+                        phone: res.data.phone,
+                        pw: res.data.pw
+                    })
+                }
+            });
             }).catch((error) => {
                 console.log(error);
                 alert('오류가 발생했습니다. 다시 시도해주세요.')
--- tdw-frontend/src/components/MyPage.vue
+++ tdw-frontend/src/components/MyPage.vue
@@ -1,17 +1,32 @@
 <template>
+    <HomeViewHeader></HomeViewHeader>
     <div>
       <h1>My Page</h1>
-      <p>User ID: {{ userId }}</p>
+      <p>User ID: {{ routeUserId }}</p>
+      <p>UserData: {{ parsedUserData }}</p>
     </div>
   </template>
   
   <script>
+  import HomeViewHeader from './Header/HomeViewHeader.vue';
+
   export default {
+    components: {
+        HomeViewHeader
+    },
       name: 'MyPage',
       computed: {
-          userId() {
-              return this.$route.params.userId;
+          routeUserId() {
+            return this.$route.params.userId;
+          },
+          parsedUserData() {
+            try {
+                return JSON.parse(this.$route.query.userData);
+            } catch (e) {
+                console.error('Error parsing userData:', e);
+                return null;
+            }
           }
-      }
+        }
   };
   </script>
